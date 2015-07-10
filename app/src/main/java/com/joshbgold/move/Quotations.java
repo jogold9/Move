151a6c8511a8f5ab709f775967e4ecdc7b7b3c8b
joshgoldeneagle <joshuabgold@gmail.com>
2015-07-09 18:12:03 -0700

    added quotes

--- app/src/main/java/com/joshbgold/move/Quotations.java
+++ app/src/main/java/com/joshbgold/move/Quotations.java
@@ -9,15 +9,14 @@ public class Quotations {
 
 //Rumi quotes and others
     public String [] quotesArray = {
-        "1st rumi quote",
-        "2nd rumi quote",
-        "1st dali lama quote",
-        "famous ant biologist quote",
-        "chimp biologist quote",
-        "hunter gatherer quote",
-        "bruce lee difficult life quote",
-        "other quote",
-        "other quote"
+        "“Only from the heart Can you touch the sky.” - Rumi",
+        "“If you want others to be happy, practice compassion. If you want to be happy, practice compassion.” - The 14th Dalai Lama",
+        "“Destroying rainforest for economic gain is like burning a Renaissance painting to cook a meal.” - E. O Wilson",
+        "“I think the best evenings are when we have messages, things that make us think, but we can also laugh and enjoy each other's company.” - " +
+                "Jane Goodall",
+        "“Our prime purpose in this life is to help others. And if you can't help them, at least don't hurt them.” - The 14th Dalai Lama",
+        "“Do not pray for an easy life, pray for the strength to endure a difficult one.” - Bruce Lee"
+
     };
 
     public String getQuote(){
