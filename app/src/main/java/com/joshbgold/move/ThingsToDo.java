998483a1dbbf42e5d6144791e48b0bdf492c5bd0
joshgoldeneagle <joshuabgold@gmail.com>
2015-07-10 10:08:57 -0700

    added more actions

--- app/src/main/java/com/joshbgold/move/ThingsToDo.java
+++ app/src/main/java/com/joshbgold/move/ThingsToDo.java
@@ -10,7 +10,12 @@ public class ThingsToDo {
     //actions to do
     public String [] ThingsToDoArray = {
             "Reach towards the sky, then reach & touch your toes (or as close as you can).  Repeat four times.", "Go " +
-            "outside", "Walk around the block.", "Take nine slow deep breaths", "Take 7 seven slow deep breaths", "Walk around the block twice."
+            "outside", "Walk around the block.", "Take nine slow deep breaths", "Take seven slow deep breaths", "Walk around the block twice.",
+            "Your choise: Go outside, or seven slow deep breaths.", "Your choice: Go outside, or go up and down some stairs.",
+            "Reach towards the sky, then reach & touch your toes (or as close as you can).  Repeat six times.",
+            "Roll both shoulders back in a circular motion.  Roll both shoulders forward.  Repeat five times.",
+            "Walk a lap or so around the office", "Stand up. Take a slow deep breath.  Sit down.  Repeat four times.",
+            "Your choice: Walk a lap or so around the office, or go outside."
     };
 
     public String getThingToDo(){
