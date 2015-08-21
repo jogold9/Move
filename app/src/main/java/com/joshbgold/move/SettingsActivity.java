24e54f497382cc8c6e27f4ee508055dba6b311c9
joshgoldeneagle <joshuabgold@gmail.com>
2015-08-21 13:17:54 -0700

    Removed separate volume class and volume layout after moving volume code to settings class

--- app/src/main/java/com/joshbgold/move/SettingsActivity.java
+++ app/src/main/java/com/joshbgold/move/SettingsActivity.java
@@ -2,28 +2,51 @@ package com.joshbgold.move;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 
 public class SettingsActivity extends Activity {
+
+    private SeekBar volumeControl = null;
+    private float volume = (float) 0.50;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        final Button chooseVolumeButton = (Button) findViewById(R.id.setReminderVolume);
+
         final Button setRepeatingInterval = (Button) findViewById(R.id.setRepeat);
         final Button backButton = (Button) findViewById(R.id.backButton);
 
-        View.OnClickListener setVolume = new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                launchSetVolumeActivity();
+        volumeControl = (SeekBar) findViewById(R.id.volumeSeekBar);
+
+        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChanged = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
             }
-        };
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+               /* Toast.makeText(SetVolumeActivity.this, "seek bar progress:" + progressChanged,
+                        Toast.LENGTH_SHORT).show();*/
+
+                volume = (float) (((double)(progressChanged))/100);  //allows division w/ decimal results instead of integer results
+                savePrefs("volume", volume);
+                Toast.makeText(SettingsActivity.this, "Audio volume is set to: " + progressChanged + " %", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         View.OnClickListener repeatInterval = new View.OnClickListener(){
             @Override
@@ -39,16 +62,9 @@ public class SettingsActivity extends Activity {
             }
         };
 
-
-        chooseVolumeButton.setOnClickListener(setVolume);
         setRepeatingInterval.setOnClickListener(repeatInterval);
         backButton.setOnClickListener(goBack);
 
-    }
-
-    void launchSetVolumeActivity(){
-        Intent intent = new Intent(SettingsActivity.this, SetVolumeActivity.class);
-        startActivity(intent);
     }
 
     void launchRepeatIntervalActivity(){
@@ -56,5 +72,12 @@ public class SettingsActivity extends Activity {
         startActivity(intent);
     }
 
+    //save prefs
+    public void savePrefs(String key, float value){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putFloat(key, value);
+        editor.commit();
+    }
 
 }
